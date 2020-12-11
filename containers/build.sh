3603a7598704761f6444665365491f8ca82d8e53
Abhinav Sharma <abhi18av@gmail.com>
2020-12-11 18:29:14 +0530

    Add the updated script for building and pushing containers

--- containers/build.sh
+++ containers/build.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
+set -uex
+
+# NOTE: Make sure you've set the environment correctly and are logged in to the registry.
+
+QUAY_NAMESPACE="seqeralabs"
+
+for container_dir in $(find * -type d); do
+  echo "Building $container_dir ..."
+  cd $container_dir
+  CONTAINER_TAG=0.0.1
+  CONTAINER_NAME=quay.io/$QUAY_NAMESPACE/$container_dir:$CONTAINER_TAG
+  docker build -t $CONTAINER_NAME .
+  CONTAINER_ID=$(docker run -d $CONTAINER_NAME)
+  docker commit $CONTAINER_ID $CONTAINER_NAME
+  docker push quay.io/$QUAY_NAMESPACE/$container_dir
+  docker stop $CONTAINER_ID
+  cd ..
+done
+
+#---------------------------------------
 
 # NOTE: An example of building and pushing multiple containers to ECR registry.
 # TODO: Please modify this script for your own use-case.
 
-REGISTRY="ecr/REGISTRY"
-for container_dir in $(find * -type d); do
-  echo "Building $container_dir ..."
-  cd $container_dir
-  aws ecr create-repository \
-    --repository-name $container_dir \
-    --image-scanning-configuration scanOnPush=false \
-    --region eu-west-1
-  docker build -t $REGISTRY/$container_dir:0.0.1 .
-  docker push $REGISTRY/$container_dir:0.0.1
-  cd ..
-done
+#AWS_REGISTRY="ecr/MY_REGISTRY"
+#AWS_REGION="MY_AWS_REGION"
+#for container_dir in $(find * -type d); do
+#  echo "Building $container_dir ..."
+#  cd $container_dir
+#  aws ecr create-repository \
+#    --repository-name $container_dir \
+#    --image-scanning-configuration scanOnPush=false \
+#    --region $MY_AWS_REGION
+#  docker build -t $AWS_REGISTRY/$container_dir:0.0.1 .
+#  docker push $AWS_REGISTRY/$container_dir:0.0.1
+#  cd ..
+#done
